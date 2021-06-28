15944ddb57d6976e538c52fc4113db007e1d321a
Collins Rollins <collinsrollins07@gmail.com>
2021-06-28 21:43:05 +0100

    third commit

--- components/Admin.js
+++ components/Admin.js
@@ -1,6 +1,6 @@
 import styles from "../components/general.module.css";
 import { useState } from "react";
-import useRouter from "next/router";
+import { useRouter } from "next/router";
 
 const Admin = () => {
   const router = useRouter();
